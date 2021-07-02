656d496baca5a7f12ec604e4fe271d032170ebca
jod35 <jodestrevin@gmail.com>
2021-07-02 13:06:36 +0300

    put and delete for recipes

--- app/api/routes.py
+++ app/api/routes.py
@@ -85,6 +85,19 @@ def get_all_users():
 
 @app.post('/signup',status_code=201)
 def create_new_user(user:UserSchema):
+    """
+    # Sign Up a user
+    - This creates a user using this schema
+    ```
+            {
+                "username":"johndoe123",
+                "email":"johndoe1234",
+                "password":"password"
+            }
+    
+    ```
+    
+    """
 
     db_user=db.query(User).filter(User.username==user.username).first()
 
@@ -108,6 +121,18 @@ def create_new_user(user:UserSchema):
 
 @app.post('/login',status_code=200)
 def login(user:UserLogin,Authorize:AuthJWT=Depends()):
+    """
+       # Login a user
+       This helps you to get access and refresh token.
+       All you need to provide is
+
+       ```
+            {
+                "username":"username",
+                "password":"password"
+            }
+       ```
+    """
     db_user=db.query(User).filter(User.username==user.username).first()
 
     if db_user and db_user.check_password(user.password):
@@ -125,6 +150,20 @@ def login(user:UserLogin,Authorize:AuthJWT=Depends()):
 
 @app.post('/recipes',status_code=status.HTTP_201_CREATED)
 def create_recipe(recipe:RecipeSchema,Authorize:AuthJWT=Depends()):
+
+    """
+        # Create a recipe 
+
+        This creates recipe and all you need is a schema
+
+        ```  
+        {        
+                "username":"johndoe123",
+                "email":"johndoe1234",
+                "password":"password"
+            }
+        ```
+    """
     try:
         Authorize.jwt_required()
     except Exception as e:
@@ -152,6 +191,11 @@ def create_recipe(recipe:RecipeSchema,Authorize:AuthJWT=Depends()):
 
 @app.get('/recipes',status_code=200,response_model=List[RecipeSchema])
 def get_all_recipes():
+    """
+        # Get all recipes
+        Returns a list of all items
+    
+    """
     recipes=db.query(Recipe).all()
 
     return recipes
@@ -160,6 +204,12 @@ def get_all_recipes():
 
 @app.get('/recipe/{recipe_id}',status_code=200,response_model=RecipeSchema)
 def get_recipe(recipe_id:int,Authorize:AuthJWT=Depends()):
+
+    """
+        # Get a recipe by its ID
+
+        It requires an ID of a recipe
+    """
 
     try:
         Authorize.jwt_required()
@@ -174,4 +224,61 @@ def get_recipe(recipe_id:int,Authorize:AuthJWT=Depends()):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Resource Not found")
     return recipe
 
+
+@app.put('/recipe/{recipe_id}',status_code=200,response_model=RecipeSchema)
+def update_a_recipe(recipe_id:int,recipe:RecipeSchema,Authorize:AuthJWT=Depends()):
+    """
+        # Update a recipe
+        - This updates a recipe by its ID
+        - You will need to provide a a schema having your update data
+
+        ```
+            {
+                "title":"title",
+                "description":"description",
+                "preparation_time":"preparation_time"
+            }
+
+        ```
     
+    """
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=401,detail="Please provide a valid token")
+
+
+    db_recipe=db.query(Recipe).filter(Recipe.id==recipe_id).first()
+
+    db_recipe.preparation_time=recipe.preparation_time
+    db_recipe.title=recipe.title
+    db_recipe.description=recipe.description
+
+    db.commit()
+
+    return db_recipe
+
+
+@app.delete('/recipe/{recipe_id}',status_code=200,response_model=RecipeSchema)
+def delete_a_recipe(recipe_id:int,Authorize:AuthJWT=Depends()):
+    """
+        # Delete a recipe
+        - All you need is to provide a recipe_id within the URL
+    """
+
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=401,detail="Please provide a valid token")
+
+    db_recipe=db.query(Recipe).filter(Recipe.id == recipe_id).first()
+
+    db.delete(db_recipe) 
+
+    db.commit()
+
+    return db_recipe   
+
+    
+
+
